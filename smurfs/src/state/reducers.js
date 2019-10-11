ead95a74169a66cac60fd6f63affb77803626383
Pere <pere.solaclaver@gmail.com>
2019-10-11 09:33:59 +0100

    Add action types and create reducers

--- smurfs/src/state/reducers.js
+++ smurfs/src/state/reducers.js
@@ -1,12 +1,38 @@
-import * as types from './actionTypes';
+import * as types from "./actionTypes";
 
 const initialCout = 0;
 
 export function countReducer(count = initialCout, action) {
-    switch (action.type) {
-        case types.INCREMENT:
-            return count + 1;
-        default: 
-        return count;
-    }
+  switch (action.type) {
+    case types.INCREMENT:
+      return count + 1;
+    default:
+      return count;
+  }
 }
+
+const initialVillage = [];
+
+export function villageReducer(village = initialVillage, action) {
+  switch (action.type) {
+    case types.ON_ADD_VILLAGE:
+      return action.payload.apiData;
+    case types.ON_ADD_NEW_SMURF:
+      return [...village, action.payload.newSmurf];
+    default:
+      return village;
+  }
+}
+
+const initialNewSmurf = {};
+
+export function newSmurfReducer(newSmurf = initialNewSmurf, action) {
+  switch (action.type) {
+    case types.ON_WRITE_NEW_SMURF:
+      return newSmurf;
+    default:
+      return newSmurf;
+  }
+}
+
+co;
